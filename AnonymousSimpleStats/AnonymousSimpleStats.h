37c6c142dbbfc577dc531aebb5ae3b50e5eaa5af
Clément Picot <clement.picot@gmail.com>
2020-05-25 15:39:36 +0200

    Initial Commit

---
+++ AnonymousSimpleStats/AnonymousSimpleStats.h
@@ -0,0 +1,19 @@
+//
+//  AnonymousSimpleStats.h
+//  AnonymousSimpleStats
+//
+//  Created by Clement Picot on 25/05/2020.
+//  Copyright © 2020 Clewig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnonymousSimpleStats.
+FOUNDATION_EXPORT double AnonymousSimpleStatsVersionNumber;
+
+//! Project version string for AnonymousSimpleStats.
+FOUNDATION_EXPORT const unsigned char AnonymousSimpleStatsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnonymousSimpleStats/PublicHeader.h>
+
+
