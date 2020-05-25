602a94ca713f473149df36dc44eae517ce2f056d
Clément Picot <clement.picot@gmail.com>
2020-05-25 17:03:22 +0200

    Add pod spec

--- AnonymousSimpleStatsTests/AnonymousSimpleStatsTests.swift
+++ AnonymousSimpleStatsTests/AnonymousSimpleStatsTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class AnonymousSimpleStatsTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        AnonymousSimpleStatsManager.shared.setup()
     }
 
     override func tearDownWithError() throws {
